b2b2a96e90a30899dd9fc5ca4029f9755b4b2da4
Muradil <muradil.kazmatov.93@inbox.com>
2017-05-01 17:32:26 +0600

    change Listing.aspx

--- GameStore/Pages/Listing.aspx.cs
+++ GameStore/Pages/Listing.aspx.cs
@@ -12,10 +12,33 @@ namespace GameStore.Pages
     public partial class Listing : System.Web.UI.Page
     {
         private Repository repository = new Repository();
+        private int pageSize = 4;
+
+        protected int CurrentPage
+        {
+            get
+            {
+                int page;
+                page = int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+                return page > MaxPage ? MaxPage : page;
+            }
+        }
+
+        // Новое свойство, возвращающее наибольший номер допустимой страницы
+        protected int MaxPage
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)repository.Games.Count() / pageSize);
+            }
+        }
 
         protected IEnumerable<Game> GetGames()
         {
-            return repository.Games;
+            return repository.Games
+                .OrderBy(g => g.GameId)
+                .Skip((CurrentPage - 1) * pageSize)
+                .Take(pageSize);
         }
 
         protected void Page_Load(object sender, EventArgs e)
